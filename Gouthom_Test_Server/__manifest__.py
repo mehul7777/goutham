32e7ec4634edb8a757a2d362c86af303d72e35cc
mehul7777 <mehul.planetodoo@gmail.com>
2021-12-04 11:24:17 +0530

    changed sale order import files

--- Gouthom_Test_Server/__manifest__.py
+++ Gouthom_Test_Server/__manifest__.py
@@ -17,6 +17,7 @@
         'wizard/Customers_Import_view.xml',
         'wizard/Products_Import_view.xml',
         'wizard/Vendor_Pricelist_Import_view.xml',
+        'wizard/Sale_Order_Import_view.xml',
         'views/Inherit_Template_view.xml',
         'views/inherit_res_partner.xml',
         'views/Inherit_sale_order_view.xml',
--- Gouthom_Test_Server/wizard/__init__.py
+++ Gouthom_Test_Server/wizard/__init__.py
@@ -1,3 +1,4 @@
 from . import Products_Import
 from . import Customers_Import
 from . import Vendor_Pricelist_Import
+from . import Sale_Order_Import
