5664fdf7a784e47071dd5904ef0e87322a0f655b
mehul7777 <mehul.planetodoo@gmail.com>
2021-12-17 18:18:38 +0530

    added id field

--- Gouthom_Test_Server/models/Inherit_res.py
+++ Gouthom_Test_Server/models/Inherit_res.py
@@ -6,3 +6,4 @@ class ResPartner(models.Model):
 
     is_a_customer = fields.Boolean(string="Is a Customer")
     is_a_vendor = fields.Boolean(string="Is a Vendor")
+    id = fields.Integer(string="ID")
