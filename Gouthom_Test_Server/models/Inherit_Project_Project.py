08978b8172964ac239614bb0f419805c75d098b6
mehul7777 <mehul.planetodoo@gmail.com>
2021-12-20 19:00:53 +0530

    changes in project files

--- Gouthom_Test_Server/models/Inherit_Project_Project.py
+++ Gouthom_Test_Server/models/Inherit_Project_Project.py
@@ -6,3 +6,13 @@ class ProProject(models.Model):
 
     allow_timesheets = fields.Boolean(string="Allow timesheets")
     allow_forecast = fields.Boolean(string="Allow forecast")
+    custom_id = fields.Integer(string="ID")
+    project_type = fields.Selection([('TIP', 'TIP'),
+                                     ('CIC', 'WIC'),
+                                     ('PIE', 'PIE'),
+                                     ('PDI', 'PDI'),
+                                     ('CAM', 'CAM'),
+                                     ('Internal-Administration', 'Internal-Administration'),
+                                     ('CON', 'CON'),
+                                     ], string="Project Type")
+    custom_created_by = fields.Many2one(comodel_name="res.users", string="Created By")
