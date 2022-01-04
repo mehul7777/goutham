c64cdd7486b76859a39dbe1c215a90a7fbfca313
mehul7777 <mehul.planetodoo@gmail.com>
2022-01-04 18:27:43 +0530

    dasd

--- Gouthom_Test_Server/models/Inherit_sale_order.py
+++ Gouthom_Test_Server/models/Inherit_sale_order.py
@@ -10,3 +10,10 @@ class SaleOrder(models.Model):
     project_end_date = fields.Date(string="Project end Date")
     point_contact = fields.Many2one(comodel_name="res.partner")
     project_manager = fields.Many2one(comodel_name="res.users")
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    product_oem_code = fields.Char(string="OEM#")
+    warehouse_id = fields.Many2one(comodel_name="stock.warehouse")
